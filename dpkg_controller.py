be13b4ea876eaab875dff9af9678c56063525b9b
nicolaskyejo <36485974+rokkafella3@users.noreply.github.com>
2019-12-21 08:10:54 +0200

    formatting and comments

--- dpkg_controller.py
+++ dpkg_controller.py
@@ -6,21 +6,23 @@ package_structure = dpkg_parser()
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def landing_page():
     """View for package names found"""
     package_names = package_structure.keys()
-    return render_template('index.html', package_names=package_names)
+    return render_template("index.html", package_names=package_names)
 
 
-@app.route('/package/<package_name>', methods=['GET'])
+@app.route("/package/<package_name>", methods=["GET"])
 def package_routes(package_name):
     """View for specific package info"""
     package_info = package_structure.get(package_name)
     if package_info is not None:
-        return render_template('package_info.html', package_name=package_name, package_info=package_info)
-    return make_response('Could not find package information...', 404)
+        return render_template(
+            "package_info.html", package_name=package_name, package_info=package_info
+        )
+    return make_response("Could not find package information...", 404)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run()
--- dpkg_status_parser.py
+++ dpkg_status_parser.py
@@ -1,12 +1,10 @@
-#!/usr/bin/env python3
-
-
 import re
 
 
-dpkg_status_file_path = './status'  # file from Ubuntu 18.04.3 LTS
-# dpkg_status_file_path = './status.real'  # file from #
-# https://gist.github.com/lauripiispanen/29735158335170c27297422a22b48caa
+dpkg_status_file_path = "./status"  # file from Ubuntu 18.04.3 LTS
+# dpkg_status_file_path = './status.real'  # file from https://gist.github.com/lauripiispanen/29735158335170c27297422a22b48caa
+description_regex = re.compile(r"^Description: (.+)")
+dependency_regex = re.compile(r"\w+\S*")
 
 
 def dpkg_parser(file=dpkg_status_file_path):
@@ -14,50 +12,59 @@ def dpkg_parser(file=dpkg_status_file_path):
        and returns a sorted dictionary with package name as the key,
        and {dependency, reverse-dependency, description} as the value
     """
-    with open(file, 'r', encoding='utf8') as f:
+    with open(file, "r", encoding="utf8") as f:
         package_info = {}
         content = f.read()
-        sections = content.split('\n\n')
+        sections = content.split("\n\n")  # Sections are separated by two new lines
         for section in sections:  # package
-            if section == '':
+            if section == "":
                 continue
             description = []
             depends_on = []
-            for line in section.split('\n'):  # info about package
-                if line == '':
+            for line in section.split("\n"):  # info about package
+                if line == "":  # skip
                     continue
-                if line.startswith('Package'):
+                if line.startswith("Package"):
                     package = line.split()[1]
                     continue
-                if line.startswith('Description'):
-                    description.append(re.search(r'^Description: (.+)', line).group(1))
+                if line.startswith("Description"):
+                    description.append(re.search(description_regex, line).group(1))
                     continue
-                if line.startswith(' /'):
+                if line.startswith(" /"):  # skip
                     continue
-                if line.startswith(' '):
+                if line.startswith(" "):
                     description.append(line)
                     continue
-                if line.startswith('Depends') or line.startswith('Pre-Depends'):
-                    for item in line.split(', '):
-                        if item.startswith('Depends') or item.startswith('Pre-Depends'):
+                if line.startswith("Depends") or line.startswith("Pre-Depends"):
+                    for item in line.split(", "):
+                        if item.startswith("Depends") or item.startswith("Pre-Depends"):
                             depends_on.append(item.split()[1])
                         else:
-                            depends_on.append(re.search(r'\w+\S*', item).group(0))
+                            depends_on.append(
+                                re.search(dependency_regex, item).group(0)
+                            )
                     depends_on.sort()
-
+            # Building data structure
             if package_info.get(package) is None:  # Is this a new package in the dict?
-                package_info[package] = {'depends': depends_on, 'reverse-depends': [], 'description': description}
+                package_info[package] = {
+                    "depends": depends_on,
+                    "reverse-depends": [],
+                    "description": description,
+                }
             else:
-                package_info[package] = {'depends': depends_on, 'reverse-depends': package_info[package]['reverse-depends'],
-                                         'description': description}
-
-            for package_dependency in package_info[package]['depends']:  # Making connections to dependencies
+                package_info[package] = {
+                    "depends": depends_on,
+                    "reverse-depends": package_info[package]["reverse-depends"],
+                    "description": description,
+                }
+            # Making connections to dependencies in order to build reverse dependency
+            for package_dependency in package_info[package]["depends"]:
                 if package_info.get(package_dependency) is None:  # is package in dict?
-                    package_info[package_dependency] = {'reverse-depends': []}
-                package_info[package_dependency]['reverse-depends'].append(package)
+                    package_info[package_dependency] = {"reverse-depends": []}
+                package_info[package_dependency]["reverse-depends"].append(package)
     sorted_package_info = {key: package_info[key] for key in sorted(package_info)}
     return sorted_package_info
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print(dpkg_parser())
